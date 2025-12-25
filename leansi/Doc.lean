aeaa81128d87f45dfe58d44de7cad44be637b105
Jonathan Rech <joni.w.rech@gmail.com>
2025-12-25 14:01:27 +0100

    Enable concat and ann in Doc

--- leansi/Doc.lean
+++ leansi/Doc.lean
@@ -1,9 +1,11 @@
+import leansi.Style
+
 namespace leansi
 
 inductive Doc (ann : Type) where
 | empty : Doc ann
 | text : String → Doc ann
--- | concat : Doc ann → Doc ann → Doc ann
--- | ann : ann → Doc ann → Doc ann
+| concat : Doc ann → Doc ann → Doc ann
+| ann : ann → Doc ann → Doc ann
 deriving Repr, Inhabited, BEq
 end leansi
