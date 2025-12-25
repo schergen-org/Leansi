5b672f4f8623aec23ad2820fb37156d6ca971da1
Jonathan Rech <joni.w.rech@gmail.com>
2025-12-25 14:02:28 +0100

    Move Styles and colors to Doc + add bg_colors - bright colors missing

--- leansi/Doc.lean
+++ leansi/Doc.lean
@@ -8,4 +8,49 @@ inductive Doc (ann : Type) where
 | concat : Doc ann → Doc ann → Doc ann
 | ann : ann → Doc ann → Doc ann
 deriving Repr, Inhabited, BEq
+
+namespace Doc
+/-- Makes text appear **bold** -/
+def bold (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann { s with bold := true }
+/-- Makes text appear <u>underlined</u> -/
+def underline (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann { s with underline := true }
+/-- Makes text appear *italic* -/
+def italic (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann { s with italic := true }
+
+-- Ansi 16 colors
+/-- Makes text appear black -/
+def black (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with fg := some 30 }
+/-- Makes text appear red -/
+def red (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with fg := some 31 }
+/-- Makes text appear green -/
+def green (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with fg := some 32 }
+/-- Makes text appear yellow -/
+def yellow (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with fg := some 33 }
+/-- Makes text appear blue -/
+def blue (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with fg := some 34 }
+/-- Makes text appear magenta -/
+def magenta (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with fg := some 35 }
+/-- Makes text appear cyan -/
+def cyan (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann { s with fg := some 36 }
+/-- Makes text appear white -/
+def white (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann { s with fg := some 37 }
+
+/-- Makes background appear black -/
+def bg_black (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with bg := some 40 }
+/-- Makes background appear red -/
+def bg_red (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with bg := some 41 }
+/-- Makes background appear green -/
+def bg_green (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with bg := some 42 }
+/-- Makes background appear yellow -/
+def bg_yellow (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with bg := some 43 }
+/-- Makes background appear blue -/
+def bg_blue (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with bg := some 44 }
+/-- Makes background appear magenta -/
+def bg_magenta (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann {s with bg := some 45 }
+/-- Makes background appear cyan -/
+def bg_cyan (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann { s with bg := some 46 }
+/-- Makes background appear white -/
+def bg_white (doc : Doc Style) (s : Style := {}) : Doc Style := doc.ann { s with bg := some 47 }
+
+end Doc
 end leansi
--- leansi/Style.lean
+++ leansi/Style.lean
@@ -15,20 +15,6 @@ deriving Repr, Inhabited, BEq
 
 namespace Style
 
-def boldStyle : Style := { bold := true }
-def underlineStyle : Style := { underline := true }
-def italicStyle : Style := { italic := true }
-
--- ANSI 16 Farben
-def black : Style := { fg := some 30 }
-def red : Style := { fg := some 31 }
-def green : Style := { fg := some 32 }
-def yellow : Style := { fg := some 33 }
-def blue : Style := { fg := some 34 }
-def magenta : Style := { fg := some 35 }
-def cyan : Style := { fg := some 36 }
-def white : Style := { fg := some 37 }
-
 -- Kombination von Styles
 def combine (s1 s2 : Style) : Style := {
   fg := s2.fg <|> s1.fg
