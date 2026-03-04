7c3f6d39667612a9e9c9554b2e0e37c75badf256
wrth1337 <mail@hellowirth.de>
2026-03-04 22:07:46 +0100

    Add proper examples.

--- Example.lean
+++ Example.lean
@@ -38,11 +38,53 @@ def main : IO Unit := do
       , Layout.hcatSep 1 [Doc.text "✓", Doc.text "Dumb Terminals" |> bright_yellow]
       , Layout.hcatSep 1 [Doc.text "✓", Doc.text "Automatic Downsampling" |> bright_magenta]
       ]
+  println colors
+  println (Doc.text "\n")
 
-  let examples :=
-    Layout.vcat
-      [ Layout.columns [10, 20] 0 [Doc.text "Colors" |> bright_red, colors] [Alignment.center, Alignment.left] true
 
-      ]
 
-  println examples
+  let styles :=
+    (Doc.text "All ansi styles: ") ++ (Doc.text "bold" |> bold) ++ Doc.text ", " ++
+    (Doc.text "dim" |> dim) ++ Doc.text ", " ++
+    (Doc.text "italic" |> italic) ++ Doc.text ", " ++
+    (Doc.text "underline" |> underline) ++ Doc.text ", " ++
+    (Doc.text "underline" |> underline) ++ Doc.text ", " ++
+    (Doc.text "strikethrough" |> strikethrough) ++ Doc.text ", " ++
+    (Doc.text "reverse" |> reverse) ++ Doc.text ", " ++
+    (Doc.text "hidden" |> hidden) ++ Doc.text ", and even " ++
+    (Doc.text "blink" |> blink) ++ Doc.text "."
+  println styles
+  println (Doc.text "\n")
+
+
+
+  let alignedText := (Doc.text "Word wrap text. Justify ") ++ (Doc.text "left" |> bright_green) ++ Doc.text ", " ++ (Doc.text "center" |> bright_yellow) ++ Doc.text ", " ++ (Doc.text "right" |> bright_blue) ++ Doc.text ", or even " ++ (Doc.text "full" |> bright_red) ++ Doc.text "."
+  let alignedTextDemo :=
+    Layout.columns [20] 3
+    [ Doc.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." |> bright_green
+    , Doc.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et." |> bright_yellow
+    , Doc.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore." |> bright_blue
+    , Doc.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna." |> bright_red
+    ]
+    [Alignment.left, Alignment.center, Alignment.right, Alignment.full] false
+  println alignedText
+  println alignedTextDemo
+  println (Doc.text "\n")
+
+
+
+  let support ← detectColorSupport
+  let supportText := (Doc.text "Detected color support: ") ++ (Doc.text s!"{support}" |> bold |> bright_cyan)
+  println supportText
+  println (Doc.text "\n")
+
+
+
+  let dims ← leansi.getTerminalDimensions
+  match dims with
+  | some (rows, cols) => println (Doc.text (s!"Terminal-Dimensions: ") ++ (Doc.text s!"{rows}" |> bright_green) ++ (Doc.text " rows, ") ++ (Doc.text s!"{cols}" |> bright_blue) ++ (Doc.text " cols"))
+  | none => println (Doc.text "Failed to detect Terminal-Dimensions" |> bright_red)
+  println (Doc.text "\n")
+
+  let more := (Doc.text "+more! \t" |> bright_red |> bold) ++ (Doc.text "Progress bars, columns, lists, and more coming soon!")
+  println more
